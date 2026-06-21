8224afd02358c62ae6441ff5192f45c7c0397377
hectowatt <w477.dev@gmail.com>
2026-06-21 21:15:20 +0900

    スクリプトをコミット

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# before.mp4 is the original video file, and after.mp4 is the output file
+ffmpeg -i before.mp4 \
+-c:v libx264 \
+-profile:v baseline \
+-level 2.1 \
+-pix_fmt yuv420p \
+-vf "scale=480:272" \
+-r 29.97 \
+-b:v 768k \
+-c:a aac \
+-profile:a aac_low \
+-ar 44100 \
+-b:a 128k \
+-movflags +faststart \
+after.mp4
